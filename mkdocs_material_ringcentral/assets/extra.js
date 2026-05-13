5f8acc856cfa6b375a07bba2ad5bf3140a374837
DaKingKong <da.kong@ringcentral.com>
2026-05-13 09:49:09 +0800

    Guard GitHub Pages redirect

--- mkdocs_material_ringcentral/assets/extra.js
+++ mkdocs_material_ringcentral/assets/extra.js
@@ -1,5 +1,7 @@
-console.log('extra.js loaded');
 if (window.location.origin === 'https://ringcentral.github.io') {
   const newUri = window.location.href.replace('ringcentral.github.io/rc-unified-crm-extension', 'appconnect.labs.ringcentral.com');
-  window.location.href = newUri;
+
+  if (newUri !== window.location.href) {
+    window.location.href = newUri;
+  }
 }
